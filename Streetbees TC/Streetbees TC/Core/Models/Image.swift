3573cf53c6b0c921e6f7e3d1c51774b0c70712d6
Bruno Miguens <brunomiguens03@hotmail.com>
2018-08-24 20:05:20 +0100

    Add comics model and view model

---
+++ Streetbees TC/Streetbees TC/Core/Models/Image.swift
@@ -0,0 +1,16 @@
+//
+//  Image.swift
+//  Streetbees TC
+//
+//  Created by Bruno Filipe Martins da Silva Miguêns on 24/08/18.
+//  Copyright © 2018 Bruno Filipe Miguêns. All rights reserved.
+//
+
+import Foundation
+
+struct ImageResult {
+    
+    let path: String
+    let fileExtension: String
+    
+}
---
+++ Streetbees TC/Streetbees TC/Core/ViewModels/ComicsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ComicsViewModel.swift
+//  Streetbees TC
+//
+//  Created by Bruno Filipe Martins da Silva Miguêns on 24/08/18.
+//  Copyright © 2018 Bruno Filipe Miguêns. All rights reserved.
+//
+
+import Foundation
